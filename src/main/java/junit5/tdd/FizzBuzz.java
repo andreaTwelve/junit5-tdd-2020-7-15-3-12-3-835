c31b60dbc32332743572fa0dfddc7b0f1d8e8b1c
XIEAN2 <andreatwelve@sina.cn>
2020-07-23 11:52:28 +0800

    [fizzbuzz] [andrea] [feature]: implement should return fizz when play fizz buzz given 3

--- src/main/java/junit5/tdd/FizzBuzz.java
+++ src/main/java/junit5/tdd/FizzBuzz.java
@@ -3,6 +3,9 @@ package junit5.tdd;
 public class FizzBuzz {
 
     public String playFizzBuzz(int number) {
+        if (number % 3 == 0) {
+            return "Fizz";
+        }
         return String.valueOf(number);
     }
 }
